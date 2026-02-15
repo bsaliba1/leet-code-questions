c576ea273a267eb97b1cc636e8770d04c5941f13
Baptiste Saliba <baptsal@gmail.com>
2026-02-15 14:33:02 -0500

    Add explanations to two problems

--- blind_75/day_01/121_best_time_to_buy_and_sell_stock.rb
+++ blind_75/day_01/121_best_time_to_buy_and_sell_stock.rb
@@ -2,6 +2,8 @@
 # Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 # Difficulty: Easy
 
+# Simple brute force approach to calculating the largest spread between all numbers
+# in the set. O(n^2) time complexity
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
@@ -15,10 +17,12 @@ def max_profit(prices)
     biggest_spread
 end
 
+# Keeps track of the smallest number and largest numbers in a given window. Once a new smaller number is hit
+# the largest and smallest number in a window is reset but the max spread between the largest and smallest numbers from the previous windows is still tracked
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit_optimized(prices)
-    spread = 0 
+    max_spread = 0 
     highest_in_range = lowest_in_range = prices[0]
     prices.each do |p|
         if p < lowest_in_range
@@ -28,23 +32,25 @@ def max_profit_optimized(prices)
         end   
         if p > highest_in_range
             highest_in_range = p
-            spread = highest_in_range - lowest_in_range if  highest_in_range - lowest_in_range > spread
+            max_spread = highest_in_range - lowest_in_range if  highest_in_range - lowest_in_range > max_spread
         end
     end
-    spread
+    max_spread
 end
 
+# A modified version of Kandane's Algorithm where the sequence of daily prices is converted into a sequence of spreads
+# Then the spreads are summed up sequentially, and a running total spread is tracked. If the current total spread is less than the individual value of a given daily spread,
+# becomes the current total spread. If at any moment the current total spread is larger than the tracked max total spread then that current total spread becomes the max
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit_kadanes_version(prices)
-    largest_spread = 0 
-    current_spread = 0
+    max_total_spread = 0 
+    current_total_spread = 0
     prices.each_index do |i|
-        next if i == 0 
-        gain = prices[i] - prices[i-1]
-        current_spread = [current_spread, 0].max
-        current_spread += gain
-        largest_spread = [current_spread, largest_spread].max
+        next if i == 0
+        spread = prices[i] - prices[i-1]
+        current_total_spread = [current_total_spread + spread, spread].max
+        max_total_spread = [current_total_spread, max_total_spread].max
     end
-    largest_spread
+    max_total_spread
 end
--- blind_75/day_01/1_two_sum.rb
+++ blind_75/day_01/1_two_sum.rb
@@ -2,6 +2,8 @@
 # Link: https://leetcode.com/problems/two-sum/
 # Difficulty: Easy
 
+# Brute force solution where you traverse the array select a value and then traverse the rest of the
+# array to find the the compliment 
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
@@ -13,3 +15,23 @@ def two_sum(nums, target)
     end
 end
 
+# First, uses a hashmap to track the indexes of each value
+# Second, iterates over the array to determine if the compliment exists for the given index
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum_optimized(nums, target)
+    h = Hash.new{|h,k| h[k] = []}
+    nums.each_with_index do |x, i|
+        h[x] << i
+    end
+    nums.each_with_index do |x, i|
+        compliment = target - x
+        if x == compliment
+            return [h[compliment][0], h[compliment][1]] if h[compliment].length > 1
+        else
+            return [h[compliment][0], i].sort if h[compliment].length > 0 
+        end 
+    end
+    return []
+end
